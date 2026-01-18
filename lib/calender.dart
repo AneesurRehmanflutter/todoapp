68428ee5f273bccfbc031ba0b9995d29e541437c
Anees ur Rehman <aneesaman890@gmail.com>
2026-01-18 17:54:02 +0500

    Update calender.dart

--- lib/calender.dart
+++ lib/calender.dart
@@ -140,7 +140,7 @@ class _CalenderState extends State<Calender> {
                                     decoration: InputDecoration(
                                       filled: true,
                                       fillColor: Color(0xff05243E),
-                                      hintText: "Anees", hintStyle: TextStyle(color: Colors.white),
+                                      hintText: "Task", hintStyle: TextStyle(color: Colors.white),
                                       border: OutlineInputBorder(
                                         borderRadius: BorderRadius.circular(10),
                                         borderSide: BorderSide.none
